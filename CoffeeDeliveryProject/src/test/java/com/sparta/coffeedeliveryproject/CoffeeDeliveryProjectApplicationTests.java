9286c32fc099dbf99faeca4591539537ad27ad35
pk_hyun <pkh954533@gmail.com>
2024-07-01 14:25:07 +0900

    개인과제 - gradle build 문제 해결을 위한 테스트 코드 수정

--- CoffeeDeliveryProject/src/test/java/com/sparta/coffeedeliveryproject/CoffeeDeliveryProjectApplicationTests.java
+++ CoffeeDeliveryProject/src/test/java/com/sparta/coffeedeliveryproject/CoffeeDeliveryProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.sparta.coffeedeliveryproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoffeeDeliveryProjectApplicationTests {
 
 	@Test
