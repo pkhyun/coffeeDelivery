362f140a6445374b636c8d5bda992c02e477d79b
pk_hyun <pkh954533@gmail.com>
2024-07-01 19:24:49 +0900

    개인과제 - 불필요한 어노테이션 제거

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/CoffeeDeliveryProjectApplication.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/CoffeeDeliveryProjectApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaAuditing
 public class CoffeeDeliveryProjectApplication {
 
